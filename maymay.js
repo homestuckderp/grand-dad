ea9e9cf622cf196adcd6034c520493f07f51201e
homestuckderp <bulbatesting@gmail.com>
2017-03-30 22:39:14 +0200

    Update maymay.js

--- maymay.js
+++ maymay.js
@@ -13,17 +13,17 @@ desc:'here be trolls.',
 engineVersion:1,
 manifest:'trollmanifest.js',
 requires:['Default dataset*'],
-sheets:{'grandDad':'http://i.imgur.com/zurxuna.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{'hemoSheet':'http://i.imgur.com/WYAXeNl.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 	new G.Res({
-		name:'Grand Dad',
-		desc:'The Grandest of all Dads.//I am not sorry, Orteil.',
+		name:'Burgundy',
+		desc:'The lowest case of troll, also known as rustbloods.//They are usually blessed with psychic powers, but are also psychically vulnearble, and lack in physical power.//Average lifespan is 40 years.'
 		startWith:1,
 		visible:true,
 		partOf:'population',
                 category:'demog',
-		icon:[5,3,'grandDad'],
+		icon:[0,0,'hemoSheet'],
 	});
 }
 });
