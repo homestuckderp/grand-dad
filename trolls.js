932afa6c065d8496eca9070a2dde02a109cf3e64
homestuckderp <bulbatesting@gmail.com>
2017-03-31 12:41:04 +0200

    Create trolls.js

---
+++ trolls.js
@@ -0,0 +1,10 @@
+
+G.AddData({
+name:'Troll mod',
+author:'Homestuck Derp',
+desc:'Homestuck trolls mod.',
+engineVersion:1,
+manifest:'trollManifest.js',
+sheets:{'hemoSheet':'http://i.imgur.com/t7xHpVi.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function()
+{
